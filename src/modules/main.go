d6c605a1922d657b4d41d63d4ca8619e4577ed47
Maikol Moreno <jdmaikol44@gmail.com>
2025-07-10 00:48:24 -0400

    Adding the feature sign-up, and make sure some users exist or the password arent the same

--- src/modules/main.go
+++ src/modules/main.go
@@ -26,6 +26,7 @@ func main() {
 
 	http.HandleFunc("/", indexHTML)
 	http.HandleFunc("/sign-in", signInHTML)
+	http.HandleFunc("/sign-up", signUpHTML)
 
 	fmt.Println("Servidor corriendo corriendo en: http://localhost:8080")
 	err := http.ListenAndServe(":8080", nil)
@@ -53,6 +54,70 @@ func indexHTML(w http.ResponseWriter, r *http.Request) {
 	if err != nil {
 		panic(err)
 	}
+}
+
+func signUpHTML(w http.ResponseWriter, r *http.Request) {
+
+	if r.Method == http.MethodPost {
+		err := r.ParseForm()
+		if err != nil {
+			http.Error(w, "Error al analiar el formulario", http.StatusBadRequest)
+			return
+		}
+
+		username := r.FormValue("user-name")
+		password := r.FormValue("user-pass")
+		conPassword := r.FormValue("user-pass-con")
+
+		userStatus := lookUpUser(username, password, conPassword)
+
+		if userStatus {
+			http.Redirect(w, r, "/", http.StatusSeeOther)
+		}
+
+	}
+
+	err := templates.ExecuteTemplate(w, "sign-up", nil)
+	if err != nil {
+		panic(err.Error())
+	}
+}
+
+func lookUpUser(username string, password string, conPassword string) bool {
+
+	authUser := false
+
+	conn := connDB()
+	query := "SELECT id_use FROM users WHERE nam_use = ?"
+	var id int
+
+	err := conn.QueryRow(query, username).Scan(&id)
+
+	if err == sql.ErrNoRows {
+		if password == conPassword {
+			query = "INSERT INTO users(nam_use, pass_use) VALUES (?, ?)"
+
+			result, err := conn.Exec(query, username, password)
+			if err != nil {
+				panic(err.Error())
+			}
+
+			lastID, err := result.LastInsertId()
+			if err != nil {
+				panic(err.Error())
+			}
+			authUser = true
+			fmt.Println("Adding user in ID:", lastID)
+		} else {
+			fmt.Println("Confirm password are the same...")
+		}
+	} else if err != nil {
+		panic(err.Error())
+	} else {
+		fmt.Println("Username no avaible...")
+	}
+
+	return authUser
 }
 
 func signInHTML(w http.ResponseWriter, r *http.Request) {
